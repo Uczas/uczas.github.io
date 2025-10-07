aedebbfe75fda569d2edecaff52d0f1c0c76153a
Ugbosu Charles <97180310+Uczas@users.noreply.github.com>
2025-10-07 22:29:17 +0100

    Update quote-section.js

--- assets/javascripts/quote-section.js
+++ assets/javascripts/quote-section.js
@@ -12,7 +12,6 @@ document.addEventListener("DOMContentLoaded", () => {
     { text: "Pure mathematics is, in its way, the poetry of logical ideas.", author: "Albert Einstein" },
     { text: "Mathematics knows no races or geographic boundaries; for mathematics, the cultural world is one country.", author: "David Hilbert" },
     { text: "Without mathematics, there’s nothing you can do. Everything around you is mathematics.", author: "Shakuntala Devi" },
-    { text: "Mathematics is not about numbers, equations, or algorithms — it's about understanding.", author: "William Paul Thurston" }
     { text: "... Good education polishes the eyes till they see too much. It sharpens the tongue till it questions why the goat must always be led to slaughter.", author: "Majekodunmi 0. Ebhohon" }
   ];
 
