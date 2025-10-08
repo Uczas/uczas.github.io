0038076d64f1153dc4ac8a1a9ad9f88bd6ec0721
Ugbosu Charles <97180310+Uczas@users.noreply.github.com>
2025-10-08 08:59:59 +0100

    Update quote-section.js

--- assets/javascripts/quote-section.js
+++ assets/javascripts/quote-section.js
@@ -9,8 +9,9 @@ document.addEventListener("DOMContentLoaded", async () => {
 
   const quotes = [
   { "text": "Pure mathematics is, in its way, the poetry of logical ideas.", "author": "Albert Einstein" },
-  { "text": "Mathematics knows no races or geographic boundaries; for mathematics, the cultural world is one country.", "author": "David Hilbert" },
+  { "text": "Mathematics is a universal language, and its truths are independent of time and place.", "author": "David Hilbert" },
   { "text": "Without mathematics, there’s nothing you can do. Everything around you is mathematics.", "author": "Shakuntala Devi" },
+  { "text": "If people do not believe that mathematics is simple, it is because they do not realize how complicated life is.", "author": "John von Neumann" },
   { "text": "... Good education polishes the eyes till they see too much. It sharpens the tongue till it questions why the goat must always be led to slaughter.", "author": "Majekodunmi O. Ebhohon" }
 ];
 
