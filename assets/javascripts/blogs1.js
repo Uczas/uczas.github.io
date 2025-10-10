4eeafc0a35f4faf4042cc46e9b54bad96c0a89e9
Ugbosu Charles <97180310+Uczas@users.noreply.github.com>
2025-10-11 00:52:58 +0100

    Update blogs1.js

--- assets/javascripts/blogs1.js
+++ assets/javascripts/blogs1.js
@@ -1,22 +1,22 @@
 // ====== BLOG DATA ======
 const blogs = [
   {
-    title: "Understanding Fixed Lifetime Inventory Models",
-    date: "October 10, 2025",
+    title: "Nigeria at 65: A New Path to Sustainability Powered by AI",
+    date: "October 1, 2025",
     description:
-      "Explore how fixed lifetime inventory models balance cost, demand, and deterioration with new optimization techniques.",
-    image: "images/blog1.jpg",
-    tags: ["Inventory", "Mathematics"],
-    link: "post1.html"
+      "Nigeria stands at a pivotal moment in its history. As a nation teeming with vibrant human capital and abundant natural resources, the quest for sustainable development is more urgent than ever. The challenges are well-known: a volatile economy, an agricultural sector yearning for modernization, an educational system in need of reinvention, and the profound environmental and economic paradox of its crude oil wealth. Layer onto this the escalating threats of climate change, seen in devastating annual floods, and a transportation network struggling to keep pace with urbanization. In the face of such complexity, a new, powerful tool is emerging that could redefine Nigeria's developmental trajectory - Artificial Intelligence.",
+    image: "/images/new-pathway.png",
+    tags: ["Nigeria at 65", "Independence", " Artificial Intelligence"],
+    link: "https://ugbosu.name.ng/blogs/nigeria-at-65-a-new-path-to-sustainability-powered-by-AI"
   },
   {
     title: "Weibull Distribution in Inventory Systems",
     date: "September 30, 2025",
     description:
-      "Learn how the Weibull distribution helps model product demand and deterioration in stochastic inventory systems.",
+      "Nigeria stands at a pivotal moment in its history. As a nation teeming with vibrant human capital and abundant natural resources, the quest for sustainable development is more urgent than ever. The challenges are well-known: a volatile economy, an agricultural sector yearning for modernization, an educational system in need of reinvention, and the profound environmental and economic paradox of its crude oil wealth. Layer onto this the escalating threats of climate change, seen in devastating annual floods, and a transportation network struggling to keep pace with urbanization. In the face of such complexity, a new, powerful tool is emerging that could redefine Nigeria's developmental trajectory - Artificial Intelligence.",
     image: "images/blog2.jpg",
     tags: ["Weibull", "Optimization"],
-    link: "post2.html"
+    link: "https://ugbosu.name.ng/blogs/nigeria-at-65-a-new-path-to-sustainability-powered-by-AI"
   },
   {
     title: "Applying Markov Decision Processes to Seasonal Demand",
