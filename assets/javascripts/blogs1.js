be645f00cc327fcb11a5f8a93ee4fa97d95e353c
Ugbosu Charles <97180310+Uczas@users.noreply.github.com>
2025-10-11 09:42:43 +0100

    Update blogs1.js

--- assets/javascripts/blogs1.js
+++ assets/javascripts/blogs1.js
@@ -6,7 +6,7 @@ const blogs = [
     description:
       "Nigeria stands at a pivotal moment in its history. As a nation teeming with vibrant human capital and abundant natural resources, the quest for sustainable development is more urgent than ever. The challenges are well-known: a volatile economy, an agricultural sector yearning for modernization, an educational system in need of reinvention, and the profound environmental and economic paradox of its crude oil wealth. Layer onto this the escalating threats of climate change, seen in devastating annual floods, and a transportation network struggling to keep pace with urbanization. In the face of such complexity, a new, powerful tool is emerging that could redefine Nigeria's developmental trajectory - Artificial Intelligence.",
     image: "/images/new-pathway.png",
-    tags: ["Nigeria at 65", "Independence", " Artificial Intelligence"],
+    tags: ["Nigeria at 65", " Artificial Intelligence"],
     link: "https://ugbosu.name.ng/blogs/nigeria-at-65-a-new-path-to-sustainability-powered-by-AI"
   }
 /*,
