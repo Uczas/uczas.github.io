c317ecd2dc0f2e7f068cdf88c469deb1f4e45c8b
Ugbosu Charles <97180310+Uczas@users.noreply.github.com>
2025-10-12 17:25:59 +0100

    Update blogs1.js

--- assets/javascripts/blogs1.js
+++ assets/javascripts/blogs1.js
@@ -5,7 +5,7 @@ const blogs = [
     date: "October 12, 2025",
     description:
       "The Federal University of Medical and Health Sciences (FUMHSK), Kwale, Delta State, has officially announced the commencement of its admission screening exercise for the 2025/2026 academic session. This specialized institution dedicated to healthcare education is now inviting applications from qualified UTME candidates who aspire to build careers in the medical and health sciences fields.",
-    image: "/images/fumhsk.jpg",
+    image: "/images/fumhsk.jpeg",
    tags: ["", ""],
     link: "https://ugbosu.name.ng/blogs/FUMHSK-Kwale-Opens-Admissions-for-2025-2026-Academic-Session"
   },
